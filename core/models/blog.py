71715b2957b383f9f995423815905552fb0b166a
SteppenHorde <steppenhorde@yandex.ru>
2019-06-20 17:11:08 +0300

    auth fixed

--- core/models/blog.py
+++ core/models/blog.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import UserManager
 from django.db import models
 
 
