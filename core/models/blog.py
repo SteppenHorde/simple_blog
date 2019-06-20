829585aae9848072808c8962c771402ce1be1e16
SteppenHorde <steppenhorde@yandex.ru>
2019-06-20 17:06:39 +0300

    add myusermanager

--- core/models/blog.py
+++ core/models/blog.py
@@ -1,17 +1,33 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+
+
+class MyUserManager(UserManager):
+    def create_user():
+        user = User()
+        blog = Blog(author=user)
+        return user
+
+
+class User(AbstractUser):
+    objects = MyUserManager()
+    def __str__(self):
+        return self.email
 
 
 class Blog(models.Model):
-    pass
+    author = OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
 
 class BlogPost(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     title = models.CharField(verbose_name='Заголовок', max_length=100)
     text = models.TextField(verbose_name='Текст', help_text='Показывается на страничке поста')
-    image = models.ImageField(upload_to='core//images', verbose_name='Картинка', blank=True, null=True)
-    pub_date = models.DateTimeField(verbose_name='Дата новости', default=timezone.now)
+    image = models.ImageField(upload_to='core/blog/images', verbose_name='Картинка', blank=True, null=True)
+    pub_date = models.DateTimeField(verbose_name='Дата', default=timezone.now)
     published = models.BooleanField(verbose_name='Опубликовать', help_text='Если установлен, то пост будет опубликован', default=False)
 
     def was_published_recently(self):
