c2d8bd874d683bfbf5c3303ff485c4496e38d4c1
SteppenHorde <steppenhorde@yandex.ru>
2019-06-22 15:03:32 +0300

    make show list of the blogs, show a blog, show a post + make subscribe button in templates

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,17 @@
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from . import views
 
 
 
 urlpatterns = [
-    path('', views.Main.as_view(), name='main'),
+    path('', views.Main.as_view(), name='main'), # тут будет персональная лента
+    path('blogs/', views.Blogs.as_view(), name='blogs'), # все блоги
+    path('blogs/<int:blog_id>', views.ShowBlog.as_view(), name='show_blog'),
+    path('blogs/<int:blog_id>/(un)subscribe', views.SubscribersManager.as_view(), name='(un)subscribe'),
+    path('blogs/<int:blog_id>/<int:post_id>', views.ShowPost.as_view(), name='show_post'),
+    path('blogs/<int:blog_id>/create', views.CreatePost.as_view(), name='create_post'),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,78 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views import generic, View
+
+from .models.blog import Blog, BlogPost
 
 
 
 class Main(TemplateView):
     template_name = 'core/main.html'
+    """
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #return render(request, self.template_name, context=context)
+        return context
+    """
+
+
+class Blogs(TemplateView):
+    template_name = 'core/blogs.html'
+
+    def get_context_data(self, *args, **kwargs):
+        all_blogs_set = Blog.objects.all()
+        context = {
+            'blogs_set': all_blogs_set,
+        }
+
+        #return render(request, self.template_name, context=context)
+        return context
+
+
+class ShowBlog(TemplateView):
+    template_name = 'core/show_blog.html'
+
+    def get_context_data(self, *args, **kwargs):
+        blog_id = kwargs['blog_id']
+        # поле author является pk для Blog, поэтому id у них совпадают:
+        blog = Blog.objects.get(author_id=blog_id)
+        posts_list = blog.blogpost_set.all()
+
+        context = {
+            'blog': blog,
+            'posts_list': posts_list,
+        }
+
+        return context
+
+
+class ShowPost(TemplateView):
+    template_name = 'core/show_post.html'
+
+    def get_context_data(self, *args, **kwargs):
+        blog_id = kwargs['blog_id']
+        post_id = kwargs['post_id']
+        # поле author является pk для Blog, поэтому id у них совпадают:
+        blog = Blog.objects.get(author_id=blog_id)
+        post = blog.blogpost_set.get(id=post_id)
+
+        context = {
+            'blog': blog,
+            'post': post,
+        }
+
+        return context
+
+
+class SubscribersManager(View):
+    pass
+    #def post(self, request, *args, **kwargs):
+        #print(args)
+        #print(kwargs)
+
+
+class CreatePost(generic.CreateView):
+    pass
+    #form_class = UserCreationForm
+    #success_url = reverse_lazy('login') # после создания поста переход на
+    #template_name = 'core/create_post.html'
